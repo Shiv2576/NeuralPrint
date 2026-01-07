76b97432e91313fafc17a055dcc2f8ec8bf40b55
Shivang <dixitshiva12358@gmail.com>
2026-01-07 18:10:37 +0530

    updated

--- app/application/page.tsx
+++ app/application/page.tsx
@@ -1,6 +1,13 @@
+// app/application/page.tsx
 "use client";
+
+import { Suspense } from "react";
 import ApplicationForm from "@/components/applicationform";
 
 export default function Page() {
-  return <ApplicationForm />;
+  return (
+    <Suspense fallback={<div>Loading application form...</div>}>
+      <ApplicationForm />
+    </Suspense>
+  );
 }
