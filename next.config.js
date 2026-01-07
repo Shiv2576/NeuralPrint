fcb43d36a25efebfb5fbfffecd434a3769cf1a51
Shivang <dixitshiva12358@gmail.com>
2026-01-07 18:39:21 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable ESLint during build
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Disable TypeScript errors during build
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Optional: Increase static page generation timeout
   staticPageGenerationTimeout: 120,
 };
 
 module.exports = nextConfig;
-
-export default nextConfig;
